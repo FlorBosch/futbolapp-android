d53d372f359f559bb73145382a3a51e72d6cac7d
Pablo Angelani <pablo.angelani@gmail.com>
2016-02-28 19:51:01 -0300

    setup BenchActivity

--- app/src/main/java/ar/com/futbolapp/ui/activity/BenchActivity.java
+++ app/src/main/java/ar/com/futbolapp/ui/activity/BenchActivity.java
@@ -1,11 +1,17 @@
 package ar.com.futbolapp.ui.activity;
 
-import android.app.Activity;
+import android.os.Bundle;
 
-/**
- * Created by Ignacio on 28/02/2016.
- */
+import ar.com.futbolapp.R;
+import ar.com.futbolapp.flows.BenchFlow;
+import flowengine.annotations.flow.FEActivity;
 
-
+@FEActivity(BenchFlow.class)
 public class BenchActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
 }

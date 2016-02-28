70d4e0a53f06e2870471e0919a4e5405b6bc38cc
Pablo Angelani <pablo.angelani@gmail.com>
2016-02-28 19:50:48 -0300

    fix BenchFlow.

--- app/src/main/java/ar/com/futbolapp/flows/BenchFlow.java
+++ app/src/main/java/ar/com/futbolapp/flows/BenchFlow.java
@@ -1,15 +1,16 @@
 package ar.com.futbolapp.flows;
 
+import ar.com.futbolapp.R;
 import ar.com.futbolapp.ui.fragment.BenchRankingFragment;
-import flowengine.annotations.flow.Flow;
+import flowengine.Flow;
 import flowengine.annotations.flow.FlowSteps;
 import flowengine.annotations.flow.Sequence;
+import flowengine.annotations.flow.StepContainer;
 
-/**
- * Created by Ignacio on 28/02/2016.
- */
 @FlowSteps(
         sequences = { @Sequence(id = "main", steps = {BenchRankingFragment.class})}
 )
-public interface BenchFlow extends Flow {
+@StepContainer(R.id.container)
+public interface BenchFlow extends Flow
+{
 }

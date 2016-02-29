c38592f538fa7c235efabd75fec3fd22398441d9
Pablo Angelani <pablo.angelani@gmail.com>
2016-02-29 00:13:08 -0300

    add profile and settings

---
+++ app/src/main/java/ar/com/futbolapp/ui/activity/ProfileActivity.java
@@ -0,0 +1,38 @@
+package ar.com.futbolapp.ui.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
+
+import ar.com.futbolapp.R;
+
+public class ProfileActivity extends AppCompatPreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+        addPreferencesFromResource(R.xml.pref_profile);
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName);
+    }
+}

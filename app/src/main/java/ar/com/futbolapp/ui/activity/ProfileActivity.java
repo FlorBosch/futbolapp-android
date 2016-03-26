cee1a94ce563928689e460bf3c22a06a3a18bf63
Pablo Angelani <pablo.angelani@gmail.com>
2016-03-25 21:27:10 -0300

    add missing layout for ProfileActivity

--- app/src/main/java/ar/com/futbolapp/ui/activity/ProfileActivity.java
+++ app/src/main/java/ar/com/futbolapp/ui/activity/ProfileActivity.java
@@ -13,7 +13,7 @@ public class ProfileActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_detail);
+        setContentView(R.layout.activity_profile);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
